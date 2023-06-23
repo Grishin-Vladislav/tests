e888ccdac34787046aec73481b997f6ca0000952
GVladislav <grishinvladislav97@gmail.com>
2023-06-23 16:12:59 +0300

    Second

--- tests/test_first.py
+++ tests/test_first.py
@@ -5,24 +5,33 @@ import pytest
 from src.first import *
 
 
-def get_full_names():
-    with open('fixtures/full_names.json') as f:
+def load_fixtures(path: str):
+    with open(path) as f:
         data = json.load(f)
     return data
 
 
-@pytest.mark.parametrize('full_names', get_full_names())
-def test_get_unique_names(full_names):
-    data = full_names['data']
-    expected = full_names['unique']
-    result = get_unique_names(data)
+@pytest.mark.parametrize('data', load_fixtures('fixtures/full_names.json'))
+def test_get_unique_names(data):
+    full_names = data['data']
+    expected = data['unique']
+    result = get_unique_names(full_names)
     assert result == expected
 
 
-@pytest.mark.parametrize('full_names', get_full_names())
-def test_get_top_3_names(full_names):
-    data = full_names['data']
-    expected = full_names['top3']
-    result = get_top_3_names(data)
+@pytest.mark.parametrize('data', load_fixtures('fixtures/full_names.json'))
+def test_get_top_3_names(data):
+    full_names = data['data']
+    expected = data['top3']
+    result = get_top_3_names(full_names)
     assert result == expected
     assert len(result) == 3
+
+
+@pytest.mark.parametrize('data', load_fixtures('fixtures/courses.json'))
+def test_get_max_min_duration_courses(data):
+    courses = data["courses"]
+    durations = data["durations"]
+    expected = data["expected"]
+    result = get_max_min_duration_courses(courses, durations)
+    assert result == expected
